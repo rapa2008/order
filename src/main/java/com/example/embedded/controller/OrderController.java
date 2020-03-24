e934cdfc59cc072ba98d333a551a21885d71f1ea
rahulpattar <rahul.pattar@gmail.com>
2020-03-24 19:26:34 +0530

    added mysql

--- src/main/java/com/example/embedded/controller/OrderController.java
+++ src/main/java/com/example/embedded/controller/OrderController.java
@@ -4,6 +4,8 @@ import com.example.embedded.model.Order;
 import com.example.embedded.repository.OrderRepository;
 import com.example.embedded.exception.ResourceNotFoundException;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,29 +23,34 @@ public class OrderController {
     @Autowired
     OrderRepository orderRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
+
     @PostMapping("/order")
     @ApiOperation(value = "Method to create an Order")
     public ResponseEntity<Order> createOrder(@RequestBody Order order){
         Date date = new Date();
         order.setCreatedDate(date);
+        logger.info("Create Order " + order);
         return new ResponseEntity<>(orderRepository.save(order), HttpStatus.CREATED);
     }
 
     @GetMapping("/order/{id}")
     @ApiOperation(value = "Method to get Order by id")
     public ResponseEntity<Order> getOrdersById(@PathVariable Long id){
+        logger.info("Query by id " + id);
         Optional<Order> findOrder = orderRepository.findById(id);
         if(findOrder.isPresent()){
             return new ResponseEntity<>(findOrder.get(), HttpStatus.OK);
         }
         else{
-            throw new ResourceNotFoundException("id=" + id);
+            throw new ResourceNotFoundException("Order id " + id + " is not found");
         }
     }
 
     @GetMapping("/order")
     @ApiOperation(value = "Method to get all Orders")
     public ResponseEntity<List<Order>> getAllOrders(){
+        logger.info("Query All Orders");
         return new ResponseEntity<>(orderRepository.findAll(), HttpStatus.OK);
     }
 
--- src/main/java/com/example/embedded/model/Order.java
+++ src/main/java/com/example/embedded/model/Order.java
@@ -10,7 +10,7 @@ import java.util.Map;
 @Entity(name="bt_order")
 public class Order {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
 
     @Convert(converter = HashMapConverter.class)
